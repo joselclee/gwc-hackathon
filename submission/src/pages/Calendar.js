1c9fd7a88f5ea1eeb019b365e4a96378ce08ea09
Martin Ha <martindn.ha@gmail.com>
2024-04-19 13:42:38 -0700

    Add initial 20 cards

--- submission/src/pages/Calendar.js
+++ submission/src/pages/Calendar.js
@@ -1,11 +1,37 @@
 import Header from '../components/Header'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 const Calendar = () => {
   return (
     <div>
         <Header />
-        Calendar page
+        <GridExample/>
     </div>
   )
 }
 
-export default Calendar
+function GridExample() {
+  return (
+    <Row xs={1} md={4} className="g-4">
+      {Array.from({ length: 20 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default Calendar;
