796ea82abee8396c3633fd480994b286420bef8a
Jose Lee <andrelee089@gmail.com>
2024-04-19 12:09:44 -0700

    change

--- submission/src/App.js
+++ submission/src/App.js
@@ -1,8 +1,16 @@
+import { Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Calendar from './pages/Calendar';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Routes>
+          <Route path="/Home" element={<Homepage />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ submission/src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+const Homepage = () => {
+  return (
+    <div className="Home">
+        <Header />
+    </div>
+  )
+}
+
+export default Homepage
