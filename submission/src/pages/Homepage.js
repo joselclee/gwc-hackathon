876ae47cdcad9cdcd047e47d6e7f1962f8283ad1
Jose Lee <andrelee089@gmail.com>
2024-04-19 14:47:11 -0700

    font and other stuff'

--- submission/src/pages/Homepage.js
+++ submission/src/pages/Homepage.js
@@ -1,11 +1,57 @@
 import Header from '../components/Header'
 import Carousel from 'react-bootstrap/Carousel'
+import './Homepage.css'
 
 const Homepage = () => {
   return (
     <div className="Home">
         <Header />
-        Homepage
+        <div className="featured">
+            Featured Events
+        </div>
+        <div className="carousel-container">
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://source.unsplash.com/1600x900/?csun"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>CSUN</h3>
+                    <p>California State University, Northridge</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        <br></br>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://source.unsplash.com/1600x900/?csun"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>CSUN</h3>
+                    <p>California State University, Northridge</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        <br></br>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="https://source.unsplash.com/1600x900/?csun"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>CSUN</h3>
+                    <p>California State University, Northridge</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        </div>
     </div>
   )
 }
